e1c20352fa03f9e10fe5ab254b350f9aebf2f3e1
jimmy-guzman <hi@jimmyguzman.com>
2021-11-18 20:15:51 -0800

    fix(typescript): 🐛 eslint's `no-shadow` should be turned off
    🏁 Closes: #19

--- src/typescript/index.spec.ts
+++ src/typescript/index.spec.ts
@@ -2,7 +2,7 @@ import typescript from './index'
 
 describe('typescript', () => {
   it('should export typescript rules config', () => {
-    expect(Object.keys(typescript.overrides[0].rules)).toHaveLength(137)
+    expect(Object.keys(typescript.overrides[0].rules)).toHaveLength(138)
     expect(typescript).toMatchSnapshot()
   })
 })
