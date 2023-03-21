0a59ea36e51eb2b6ee7cf92944162cef191b1baa
jimmy-guzman <hi@jimmyguzman.com>
2023-03-20 19:56:12 -0500

    feat(ts): ✨ enable a few TypeScript rules & support TypeScript v5
    * enable `no-import-type-side-effects`
    * enable `no-mixed-enums`
    * enable `no-unsafe-declaration-merging`
    * enable `sort-type-constituents`
    
    BREAKING CHANGE: 💥 new enabled rules can potentially require code changes

--- src/typescript/index.spec.ts
+++ src/typescript/index.spec.ts
@@ -2,7 +2,7 @@ import typescript from './index'
 
 describe('typescript', () => {
   it('should export typescript rules config', () => {
-    expect(Object.keys(typescript.overrides[0].rules)).toHaveLength(144)
+    expect(Object.keys(typescript.overrides[0].rules)).toHaveLength(151)
     expect(typescript).toMatchSnapshot()
   })
 })
