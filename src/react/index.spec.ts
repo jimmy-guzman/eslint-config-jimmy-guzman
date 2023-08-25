e23c722b595b71b01c760795878b057a16abd8ad
jimmy-guzman <hi@jimmyguzman.com>
2023-08-24 20:13:08 -0500

    feat(react): ✨ bump `eslint-plugin-jsx-a11y` to v6.7.1

--- src/react/index.spec.ts
+++ src/react/index.spec.ts
@@ -2,7 +2,7 @@ import react from './index'
 
 describe('react', () => {
   it('should export react rules config', () => {
-    expect(Object.keys(react.rules)).toHaveLength(117)
+    expect(Object.keys(react.rules)).toHaveLength(120)
     expect(react).toMatchSnapshot()
   })
 })
