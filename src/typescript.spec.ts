470dcbc617612d4e6c51a90737b466d0a6a68668
jimmy-guzman <hi@jimmyguzman.com>
2023-12-31 13:32:40 -0800

    fix(ts): 🐛 disable `dot-notation`
    🏁 Closes: #216

--- src/typescript.spec.ts
+++ src/typescript.spec.ts
@@ -2,7 +2,7 @@ import typescript from './typescript'
 
 describe('typescript', () => {
   it('should export typescript rules config', () => {
-    expect(Object.keys(typescript.overrides[0].rules)).toHaveLength(156)
+    expect(Object.keys(typescript.overrides[0].rules)).toHaveLength(157)
     expect(typescript).toMatchSnapshot()
   })
 })
