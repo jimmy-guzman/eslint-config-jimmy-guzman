96aa7879a411a900c80564b3802bd8021904c0d5
jimmy-guzman <hi@jimmyguzman.com>
2023-12-30 16:15:13 -0800

    feat(base): ✨ add more rules and bump `eslint`
    added `no-empty-static-block`, `no-new-native-nonconstructor` &
    `no-object-constructor`
    
    BREAKING CHANGE: 💥 new rules can potentially require code changes,
    `eslint` v8.50.0 is required

--- src/errors/index.spec.ts
+++ src/errors/index.spec.ts
@@ -2,7 +2,7 @@ import errors from './index'
 
 describe('errors', () => {
   it('should export errors rules config', () => {
-    expect(Object.keys(errors.rules)).toHaveLength(69)
+    expect(Object.keys(errors.rules)).toHaveLength(70)
     expect(errors).toMatchSnapshot()
   })
 })
