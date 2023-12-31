dbf9da93cb843b18c8a4967ff1c7762f45d7d43c
jimmy-guzman <hi@jimmyguzman.com>
2023-12-30 17:50:21 -0800

    feat(*): ✨ add new `jest/no-confusing-set-timeout` rule
    BREAKING CHANGE: 💥 new rule might require code changes

--- src/vitest/index.spec.ts
+++ src/vitest/index.spec.ts
@@ -2,7 +2,7 @@ import jest from './index'
 
 describe('vitest', () => {
   it('should export vitest rules config', () => {
-    expect(Object.keys(jest.overrides[0].rules)).toHaveLength(56)
+    expect(Object.keys(jest.overrides[0].rules)).toHaveLength(57)
     expect(jest).toMatchSnapshot()
   })
 })
