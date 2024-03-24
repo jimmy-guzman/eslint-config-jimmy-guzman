aad32c47653ea6a9b7d9249b537194fd187b26a9
jimmy-guzman <hi@jimmyguzman.com>
2024-03-24 16:24:30 -0500

    feat(ts): ✨ `typescript` v5.4 is now supported
    BREAKING CHANGE: 💥 `eslint` of at least v8.56.0 is required and
    `typescript` of at least v4.7.4 is required.

--- src/typescript.spec.ts
+++ src/typescript.spec.ts
@@ -4,7 +4,7 @@ describe('typescript', () => {
   it('should export typescript rules config', () => {
     const [{ rules = {} }] = typescript.overrides
 
-    expect(Object.keys(rules)).toHaveLength(157)
+    expect(Object.keys(rules)).toHaveLength(164)
     expect(typescript).toMatchSnapshot()
   })
 })
